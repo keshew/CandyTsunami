e161f9c9d1d5cb493e101e9c5253ec1d8b895fd8
keshew <artyom.korotckov2017@yandex.ru>
2025-02-14 18:52:17 +0500

    Initial Commit

---
+++ CandyTsunami/CandyTsunamiApp.swift
@@ -0,0 +1,17 @@
+//
+//  CandyTsunamiApp.swift
+//  CandyTsunami
+//
+//  Created by Артём Коротков on 14.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct CandyTsunamiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
